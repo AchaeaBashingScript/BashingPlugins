3ee56122dca4aacf5cc3e7337d7a9dc15d44dff7
PhoenixCodes <regoldberg@gmail.com>
2020-10-18 13:42:22 -0700

    Update ManuallyAddPossibleTarget.lua

--- ManuallyAddPossibleTarget.lua
+++ ManuallyAddPossibleTarget.lua
@@ -1,6 +1,9 @@
-tempAlias("^kconfig bashing addTarget \\w*?(\\d+)$", [[
+tempAlias("^kconfig bashing addTarget (.+)$", [[
   for _, item in ipairs(keneanung.bashing.room) do
-    if item.id == matches[2] then
+    if item.id == matches[2]:match("%d+$") then
+      keneanung.bashing.addPossibleTarget(item.name)
+      return
+    elseif item.name == matches[2]:match("^%D+") then
       keneanung.bashing.addPossibleTarget(item.name)
       return
     end
